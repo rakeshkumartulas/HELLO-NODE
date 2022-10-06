1e82f9f899cef1ba4192a4059ba836e9e2ed25eb
rakeshkumartulas <113280390+rakeshkumartulas@users.noreply.github.com>
2022-10-06 15:15:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-// Importing http core package/module provided by nodejs
-
-var http = require('http');
-
-// Creating a server
-
-http.createServer(function (req, res) {
-
-// Write response as Html(text)
-
-res.writeHead(200, {'Content-Type': 'text/html'});
-
-// Writing static text
-
-res.end('Hello Node.js');
-
-
-
-}).listen(4200);
+let hello="Hello";
+returnHello=()=>{
+  console.log(hello);
+}
+returnHello();
