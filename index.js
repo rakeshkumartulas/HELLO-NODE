2e252750fe4a4056fefe60e77b3d9e161f43f722
rakeshkumartulas <113280390+rakeshkumartulas@users.noreply.github.com>
2022-10-06 15:33:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-Console.log("hello");
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello Node.js!');
+}).listen(8080);
